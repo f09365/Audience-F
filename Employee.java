aac77ae25464daff8ec475a6dd3d1505c214948c
F <f09365@gmail.com>
2020-03-14 14:04:25 +0800

    2nd commit

---
+++ Employee.java
@@ -0,0 +1,34 @@
+package jquery_ajax.bean;
+
+public class Employee {
+	private int empno;
+	private String ename;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(int empno, String ename) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	
+}
